622feda1af390561128bce13b0359b98a702c6d3
sabine <sabine.robart@protonmail.com>
2021-07-21 15:19:08 +0200

    feat: Add 404

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - maybe checkout our
+        <router-link to="/coaches">Awesome Coaches</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>

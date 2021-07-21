af44c192e3c8f0c616f4a69b968843539b187001
sabine <sabine.robart@protonmail.com>
2021-07-21 12:27:15 +0200

    feat: Save coach registration

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,22 @@
 export default {
-	registerCoach(context, data) {
+	async registerCoach(context, data) {
+		const userId = context.rootGetters.userId;
 		const coachData = {
-			id: context.rootGetters.userId,
 			firstName: data.first,
 			lastName: data.last,
 			description: data.desc,
 			hourlyRate: data.rate,
 			areas: data.areas
 		};
-		context.commit('registerCoach', coachData)
+
+		const res = await fetch(`https://find-a-coach-2494d-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`, {
+			method: 'PUT', // !POST because the id will already exist when we add authentication
+			body: JSON.stringify(coachData)
+		})
+		// const resData = await res.json();
+		if (!res.ok) {
+			// error ..
+		}
+		context.commit('registerCoach', { ...coachData, id: userId })
 	}
 }

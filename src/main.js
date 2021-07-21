9c851576c1f1673ce2d9d45e69a9f7837f3d7ab5
sabine <sabine.robart@protonmail.com>
2021-07-21 14:26:07 +0200

    feat: Add Loader + Dialog components

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store/index.js'
 import BaseCard from './components/UI/BaseCard'
 import BaseButton from './components/UI/BaseButton'
 import BaseBadge from './components/UI/BaseBadge'
+import BaseSpinner from './components/UI/BaseSpinner'
+import BaseDialog from './components/UI/BaseDialog'
 
 const app = createApp(App)
 app.use(router);
@@ -12,4 +14,6 @@ app.use(store);
 app.component('base-card', BaseCard)
 app.component('base-button', BaseButton)
 app.component('base-badge', BaseBadge)
+app.component('base-spinner', BaseSpinner)
+app.component('base-dialog', BaseDialog)
 app.mount('#app')

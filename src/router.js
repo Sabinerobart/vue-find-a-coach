68f6908c048af1d14ffb2a974945106924553e0e
sabine <sabine.robart@protonmail.com>
2021-07-24 14:10:43 +0200

    feat: Add Route Guards

--- src/router.js
+++ src/router.js
@@ -8,21 +8,33 @@ import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
 import Auth from './pages/auth/UserAuth.vue';
 
+import store from './store/index';
+
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: '/', redirect: '/auth' },
-        { path: '/auth', component: Auth },
-        { path: '/coaches', component: CoachesList },
-        {
-            path: '/coaches/:id', component: CoachDetails, props: true, children: [
-                { path: 'contact', component: ContactCoach } // /coaches/c1/contact
-            ]
-        },
-        { path: '/registration', component: CoachRegistration },
-        { path: '/requests', component: RequestsReceived },
-        { path: '/:notFound(.*)', component: NotFound },
-    ],
+	history: createWebHistory(),
+	routes: [
+		{ path: '/', redirect: '/auth' },
+		{ path: '/auth', component: Auth, meta: { requiresUnAuth: true } },
+		{ path: '/coaches', component: CoachesList },
+		{
+			path: '/coaches/:id', component: CoachDetails, props: true, children: [
+				{ path: 'contact', component: ContactCoach } // /coaches/c1/contact
+			]
+		},
+		{ path: '/registration', component: CoachRegistration, meta: { requiresAuth: true } },
+		{ path: '/requests', component: RequestsReceived, meta: { requiresAuth: true } },
+		{ path: '/:notFound(.*)', component: NotFound },
+	],
 });
+
+router.beforeEach((to, _, next) => {
+	if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+		next('/auth');
+	} else if (to.meta.requiresUnAuth && store.getters.isAuthenticated) {
+		next('/coaches');
+	} else {
+		next();
+	}
+})
 
 export default router;
